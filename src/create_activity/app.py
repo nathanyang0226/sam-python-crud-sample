b92f79b7dca4fd7240abb680621f4b5f25c7d054
unknown <PB085656@daikin.net.cn>
2023-01-09 16:46:37 +0800

    update files

--- src/create_activity/app.py
+++ src/create_activity/app.py
@@ -1,9 +1,14 @@
 import boto3
+import sys
 import os
 import json
 import uuid
+import logging
+import traceback
 from datetime import datetime
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(message, context):
 
@@ -15,39 +20,57 @@ def lambda_handler(message, context):
             'body': json.dumps({'msg': 'Bad Request'})
         }
 
-    table_name = os.environ.get('TABLE', 'Activities')
-    region = os.environ.get('REGION', 'us-east-1')
-    aws_environment = os.environ.get('AWSENV', 'AWS')
 
-    if aws_environment == 'AWS_SAM_LOCAL':
-        activities_table = boto3.resource(
-            'dynamodb',
-            endpoint_url='http://dynamodb:8000'
+    '''Simple Lambda function add logger component'''
+    try:
+        logger.info(f'message: {message}')
+
+        table_name = os.environ.get('TABLE', 'Activities')
+        region = os.environ.get('REGION', 'cn-northwest-1')
+        aws_environment = os.environ.get('AWSENV', 'AWS')
+
+        if aws_environment == 'AWS_SAM_LOCAL':
+            activities_table = boto3.resource(
+                'dynamodb',
+                endpoint_url='http://dynamodb:8000'
+            )
+        else:
+            activities_table = boto3.resource(
+                'dynamodb',
+                region_name=region
+            )
+
+        table = activities_table.Table(table_name)
+        activity = json.loads(message['body'])
+
+        params = {
+            'id': str(uuid.uuid4()),
+            'date': str(datetime.timestamp(datetime.now())),
+            'stage': activity['stage'],
+            'description': activity['description']
+        }
+
+        response = table.put_item(
+            TableName=table_name,
+            Item=params
         )
-    else:
-        activities_table = boto3.resource(
-            'dynamodb',
-            region_name=region
-        )
+        print(response)
 
-    table = activities_table.Table(table_name)
-    activity = json.loads(message['body'])
+        return {
+            'statusCode': 201,
+            'headers': {},
+            'body': json.dumps({'msg': 'Activity created'})
+        }
 
-    params = {
-        'id': str(uuid.uuid4()),
-        'date': str(datetime.timestamp(datetime.now())),
-        'stage': activity['stage'],
-        'description': activity['description']
-    }
+      
+    except Exception as exp:
+        exception_type, exception_value, exception_traceback = sys.exc_info()
+        traceback_string = traceback.format_exception(exception_type, exception_value, exception_traceback)
+        err_msg = json.dumps({
+            "errorType": exception_type.__name__,
+            "errorMessage": str(exception_value),
+            "stackTrace": traceback_string
+        })
+        logger.error(err_msg)
 
-    response = table.put_item(
-        TableName=table_name,
-        Item=params
-    )
-    print(response)
-
-    return {
-        'statusCode': 201,
-        'headers': {},
-        'body': json.dumps({'msg': 'Activity created'})
-    }
+    
